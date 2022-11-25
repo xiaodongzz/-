a620e8ddaf4b10fdfc591626aa5eaa5ada366bfc
ZXD <1399176533@qq.com>
2022-11-25 15:10:28 +0800

    20221125

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+E:
+<<<<<<< HEAD
+cd \博士 2021\博士课题 ZXD
+=======
+cd \博士 2021\0 智能家用健康管理机器人 ZXD
+>>>>>>> 85ecac3... 2022/11/24 鍛ㄥ洓 10:51:20.19
+git add -A
+git commit -m "%date% %time%"
+git push -u origin master
+pause
